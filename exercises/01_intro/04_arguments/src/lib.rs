64051c7f6ad3c2759109290b57642e8752473427
LundyBernard <lundy.bernard@gmail.com>
2025-02-05 16:11:10 -0800

    solve: (01) intro - (04) arguments

--- exercises/01_intro/04_arguments/src/lib.rs
+++ exercises/01_intro/04_arguments/src/lib.rs
@@ -3,7 +3,9 @@ use pyo3::prelude::*;
 #[pyfunction]
 // TODO: Define a function that takes as input a vector of unsigned integers
 //  and prints each number in the list.
-fn print_number_list() {}
+fn print_number_list(list: Vec<u32>) {
+    for n in &list { println!("{n}");}
+}
 
 #[pymodule]
 fn arguments(m: &Bound<'_, PyModule>) -> PyResult<()> {
