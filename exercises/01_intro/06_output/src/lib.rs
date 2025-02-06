0c689f19a071d190338e4c9b275a5ded8c48014a
LundyBernard <lundy.bernard@gmail.com>
2025-02-05 17:35:30 -0800

    solve: (01) intro - (06) output

--- exercises/01_intro/06_output/src/lib.rs
+++ exercises/01_intro/06_output/src/lib.rs
@@ -1,10 +1,24 @@
 use pyo3::prelude::*;
 
+// I'm a bit surprised this works.  guessing PyResult performs the conversion
+// from Vec<u64> to a Python List object
 #[pyfunction]
-// TODO: Implement a function that returns a list containing the first `n` numbers in Fibonacci's sequence.
-fn fibonacci(/* n: TODO */) /* -> TODO */ {
-    todo!()
+fn fibonacci(n: usize) -> Vec<u64> {
+    match n {
+        0 => Vec::new(),
+        1 => vec![0],
+        2 => vec![0, 1],
+        _ => {
+            let mut sequence = vec![0, 1];
+            for i in 2..n {
+                let next = sequence[i - 1] + sequence[i - 2];
+                sequence.push(next);
+            }
+            sequence
+        }
+    }
 }
+
 
 #[pymodule]
 fn output(m: &Bound<'_, PyModule>) -> PyResult<()> {
