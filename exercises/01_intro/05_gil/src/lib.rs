aabaaef5575511a9dab02c36e09606f9d582f3b6
LundyBernard <lundy.bernard@gmail.com>
2025-02-05 16:40:57 -0800

    solve: (01) intro - (05) gil

--- exercises/01_intro/05_gil/src/lib.rs
+++ exercises/01_intro/05_gil/src/lib.rs
@@ -1,11 +1,16 @@
 use pyo3::prelude::*;
+use pyo3::types::PyList;
+
 
 #[pyfunction]
 // TODO: Use `PyList` instead of `Vec<u64>` as the input type. Panic on errors, for now.
 // You might find this useful: https://pyo3.rs/v0.22.0/conversions/traits#extract-and-the-frompyobject-trait
-fn print_number_list() {
-    todo!()
+fn print_number_list(lst: Bound<'_, PyList>){
+    for n in lst {
+        println!("{n}")
+    }
 }
+
 
 #[pymodule]
 fn gil(m: &Bound<'_, PyModule>) -> PyResult<()> {
