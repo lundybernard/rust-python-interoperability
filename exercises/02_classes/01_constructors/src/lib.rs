8d5519fb42b90706a11a294331e2a96a344afd18
LundyBernard <lundy.bernard@gmail.com>
2025-02-07 18:12:24 -0800

    solve: (02) classes - (01) constructors

--- exercises/02_classes/01_constructors/src/lib.rs
+++ exercises/02_classes/01_constructors/src/lib.rs
@@ -1,4 +1,6 @@
 use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
 
 // TODO: Add a `__new__` constructor to the `ShoppingOrder` class that takes the following arguments:
 //  - `name` (non-empty string)
@@ -11,9 +13,27 @@ struct ShoppingOrder {
     #[pyo3(get)]
     name: String,
     #[pyo3(get)]
-    price: u64,
+    price: i64,
     #[pyo3(get, set)]
-    quantity: u64,
+    quantity: i64,
+}
+
+#[pymethods]
+impl ShoppingOrder {
+    #[new]
+    fn new(name: String, price: i64, quantity: i64) -> PyResult<Self> {
+        if name.is_empty() {
+            return Err(PyValueError::new_err("Name cannot be empty"));
+        }
+        if price <= 0 {
+            return Err(PyValueError::new_err("Price must be positive"));
+        }
+        if quantity <= 0 {
+            return Err(PyValueError::new_err("Quantity must be positive"));
+        }
+
+        Ok(Self { name, price, quantity })
+    }
 }
 
 #[pymodule]
