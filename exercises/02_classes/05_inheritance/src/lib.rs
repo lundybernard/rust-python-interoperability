7090f385eeac902d9a9d6fe745c963f80e64459c
LundyBernard <lundy.bernard@gmail.com>
2025-02-08 17:40:01 -0800

    solve: (02) classes - (05) inheritance

--- exercises/02_classes/05_inheritance/src/lib.rs
+++ exercises/02_classes/05_inheritance/src/lib.rs
@@ -8,7 +8,51 @@
 //  attributes of an `Employee`.
 use pyo3::prelude::*;
 
+
+#[pyclass(subclass)]
+struct Person {
+    #[pyo3(get)]
+    first_name: String,
+    #[pyo3(get)]
+    last_name: String,
+}
+
+#[pyclass(extends=Person)]
+struct Employee {
+    #[pyo3(get)]
+    id: u32,
+}
+
+
+#[pymethods]
+impl Person {
+    #[new]
+    fn new(first_name: String, last_name: String) -> Self {
+        Self { first_name, last_name }
+    }
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
+#[pymethods]
+impl Employee {
+    #[new]
+    fn new(
+        first_name: String,
+        last_name: String,
+        id: u32,
+    ) -> PyClassInitializer<Self> {
+        let sup = Person::new(first_name, last_name);
+        let child = Self { id };
+        PyClassInitializer::from(sup).add_subclass(child)
+    }
+}
+
+
 #[pymodule]
 fn inheritance(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Person>()?;
+    m.add_class::<Employee>()?;
     Ok(())
 }
